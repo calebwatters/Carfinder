dc1a84f6e8531a686cdbd0715b19fa69e5437a8e
github username <github email address>
2019-05-28 15:37:12 -0700

    added navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_05_28_200835) do
-
 
   create_table "car_makes", force: :cascade do |t|
     t.integer "car_post_id"
