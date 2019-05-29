b831b336d52a894d60fd95bd988894129aecb4ad
github username <github email address>
2019-05-29 09:38:18 -0700

    added some login functionality. Sessions controller was added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :users
   resources :make_models
   resources :car_makes
   resources :car_posts
   resources :reviews
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
