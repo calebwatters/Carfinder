8ea84907921f64bb01878be9270d094cf325e696
github username <github email address>
2019-05-31 07:59:41 -0700

    modified drop down

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ CarMake.destroy_all
 Review.destroy_all
 MakeModel.destroy_all
 
-100.times do 
+1000.times do 
     MakeModel.create(
     make_model: Faker::Vehicle.make_and_model,
     year: Faker::Vehicle.year,
